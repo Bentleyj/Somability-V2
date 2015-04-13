28e5d6b1353e43e25bd688fb32a95c3f62420ed2
peted <peted70@hotmail.com>
2015-04-13 14:31:01 +0100

    initial add

---
+++ pch.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <wrl/client.h>
+#include <d3d11_2.h>
+#include <d2d1_1.h>
+#include <d2d1effects.h>
+#include <dwrite_1.h>
+#include <wincodec.h>
+#include <agile.h>
+#include <DirectXMath.h>
+#include "App.xaml.h"
+
+#include <collection.h>
+#include <ppltasks.h>
+
+#include "Common\NavigationHelper.h"
+#include "Common\RelayCommand.h"
+#include "Common\SuspensionManager.h"
+#include "DataModel\SampleDataSource.h"
+#include "App.xaml.h"
